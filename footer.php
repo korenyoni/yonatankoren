1c6199db32ab6ee0eccbf69e088cd996db1af836
Yonathan Koren <yonkornilov@live.com>
2018-04-02 19:27:04 -0400

    update footer to contain links and sit nicely

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 $year = date('Y');
 echo <<<HTML
 <div class="twelve columns" style="margin-top: 5%; text-align: center">
-    <h6>Yonathan Koren {$year}</h6> Made using Skeleton and Parsedown.
+    <h6>Yonathan Koren {$year}. Made using <a href="https://github.com/dhg/Skeleton">Skeleton</a> and <a href="https://github.com/erusev/parsedown">Parsedown</a>.</h6>
 </div>
 HTML;
 ?>
