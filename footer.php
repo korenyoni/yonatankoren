a9ea0b9ee6634f68c2de231279197b3cf4282700
Yonathan Koren <yonkornilov@live.com>
2018-04-02 19:19:55 -0400

    give credit in footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 $year = date('Y');
 echo <<<HTML
 <div class="twelve columns" style="margin-top: 5%; text-align: center">
-    <h6>Yonathan Koren {$year}</h6>
+    <h6>Yonathan Koren {$year}</h6> Made using Skeleton and Parsedown.
 </div>
 HTML;
 ?>
